fcb95a685aa839760ecc6821f256fca3943e1dee
Roberto Garcia <pablo.garcia@cua.uam.mx>
2024-12-02 19:11:51 -0600

    Se configuro la conexion a BD alumnos en el archivo config.py, ademas se añadio como comentario las consultas que se deben hacer en repo.py

--- ORM/config.py
+++ ORM/config.py
@@ -1,1 +1,33 @@
 # Configurar conexion a BD
+from sqlalchemy import create_engine # para crear el engine (conexion a BD)
+from sqlalchemy.orm import sessionmaker # para crear sesiones cuando se hagan consultas
+from sqlalchemy.ext.declarative import declarative_base # para crear BaseClass y poder mapear tablas
+
+# 1.- Configuracion de BD
+# --> nombre_servidor://usuario:contraseña_del_usuario@URL_SERVIDOR:puerto/nombreBD
+URL_BASE_DATOS = "postgresql://usuario-ejemplo:prest@localhost:5432/alumnos"
+
+
+# 2.- conectarse con create_engine al esquema "app" (=app)
+engine= create_engine(URL_BASE_DATOS,
+                      connect_args={
+                          "options":"-csearch_path= app"  # -csearch_path= Nombre del Esquema
+                      })
+
+
+# 3.- Clase que permite crear objetos tipo session para las query
+SessionClass= sessionmaker(engine)
+
+
+# 4.- Funcion para objetos clase tipo SessionClass
+def generador_sesion(): # generar sesiones al hacer querys
+    sesion= SessionClass()
+    try:
+        yield sesion # es como un "return sesion" 
+    finally:
+        sesion.close() # cerrar sesion al terminar query
+
+
+# 5.- Clase base para mapear tablas
+BaseClass = declarative_base()
+
--- ORM/repo.py
+++ ORM/repo.py
@@ -1,1 +1,14 @@
 # Implementar consultas sql SELECT y DELETE
+
+
+# SELECT * FROM app.alumnos
+# SELECT * FROM app.alumnos WHERE id={id_al}
+# SELECT * FROM app.fotos
+# SELECT * FROM app.fotos WHERE id={id_fo}
+# SELECT * FROM app.fotos WHERE id_alumnos={id_al}
+# SELECT * FROM app.calificaciones
+# SELECT * FROM app.calificaciones WHERE id={id_fo}
+# SELECT * FROM app.calificaciones WHERE id_alumnos={id_al}
+# DELETE FROM app.alumnos WHERE id_alumnos={id_al}
+# DELETE FROM app.calificaciones WHERE id_alumnos={id_al}
+# DELETE FROM app.fotos WHERE id_alumnos={id_al}
