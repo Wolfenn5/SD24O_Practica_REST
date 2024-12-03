ed833cecb957015732cafae9e7b477552575402e
Roberto Garcia <pablo.garcia@cua.uam.mx>
2024-12-02 22:37:13 -0600

    Se añadieron las configuraciones iniciales de api.py

--- api.py
+++ api.py
@@ -11,3 +11,62 @@
 # delete("/alumnos/{id}/calificaciones")
 # delete("/alumnos/{id}/fotos")
 # delete("/alumnos/{id})
+from fastapi import FastAPI, UploadFile, File, Form, Depends # para manejo de archivos, formularios html y sesiones
+from typing import Optional # para hacer que los argumentos señalados sean opcionales
+
+from ORM.config import generador_sesion # generador de sesiones 
+import ORM.repo as repo # funciones para hacer consultas a la BD del archivo repo.py
+from sqlalchemy.orm import Session # para gestionar sesiones al hacer querys
+from sqlalchemy import and_ # para utilizar operador and al hacer querys
+
+
+# conda create --name prest               --> Para crear ambiente
+# conda activate prest                    --> Para activar ambiente
+# python -m uvicorn api:app --reload      --> Para levantar el servidor
+
+
+
+
+# creación del servidor
+app = FastAPI()
+
+
+# decorador (actua como un home en el servidor)
+@app.get("/")
+def hola_mundo():
+    print("invocando a ruta /")
+    respuesta = {
+        "mensaje": "Practica Rest SD24O"
+    }
+
+    return respuesta
+
+
+#########################
+
+# Alumnos
+
+#########################
+
+
+
+
+
+
+
+#########################
+
+# Calificaciones
+
+#########################
+
+
+
+
+
+
+#########################
+
+# Fotos
+
+#########################
