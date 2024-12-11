020db612ce68713e9e350728ce688eaf9e249e77
Roberto Garcia <pablo.garcia@cua.uam.mx>
2024-12-11 08:33:13 -0600

    Se añadio y configuro esquemas.py

---
+++ ORM/esquemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+# ---> Este archivo esquemas.py sirve para asociar las tablas de la BD, donde se recibe lo que el cliente mande
+
+
+# No se inlcuye la ruta, el id ni la fecha de registro porque no se quiere que el usuario pueda modificarlos
+
+# Definir el esquema usuario
+class UsuarioBase(BaseModel):
+    nombre:str
+    edad:int
+    domicilio:str
+    carrera:str
+    trimestre:str
+    email:str
+    password:str
+
+
+# Definir el esquema Calificacion
+class CalificacionBase(BaseModel):
+    uea:str
+    calificacion:str
+
+# Definir el esquema fotos
+class FotosBase(BaseModel):
+    titulo:str
+    descripcion:str
